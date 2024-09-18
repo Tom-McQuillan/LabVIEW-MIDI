fcd2cfce63cc3689ada732e6d053a033e185a773
Tom McQuillan <104849466+Tom-McQuillan@users.noreply.github.com>
2024-09-18 17:13:50 +0100

    I got it working with filtering MIDI Events!

--- cpp-src/MIDIInToLabVIEWWrapper/MIDIInToLabVIEWWrapper.cpp
+++ cpp-src/MIDIInToLabVIEWWrapper/MIDIInToLabVIEWWrapper.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <string>
 #include "extcode.h"
+#include <vector>
+#include <algorithm>  // for std::find
 
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "labviewv.lib")
@@ -18,19 +20,39 @@ struct MidiData {
 // Pointer to the LabVIEW User Event reference
 static LVUserEventRef* g_userEventRef = nullptr;
 
+// Array of Status messages to filter
+static std::vector<uint8_t> statusFilterArray;
+
 // Callback function for MIDI input
 void CALLBACK MidiInCallbackFunction(HMIDIIN hMidiIn, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
 {
     if (g_userEventRef && *g_userEventRef) {
         // Extract MIDI data
-        MidiData midiData;
-        midiData.Status = (int32_t)(dwParam1 & 0xFF);
-        midiData.data1 = (int32_t)((dwParam1 >> 8) & 0xFF);
-        midiData.data2 = (int32_t)((dwParam1 >> 16) & 0xFF);
+        int32_t status = (int32_t)(dwParam1 & 0xFF);
+        int32_t data1 = (int32_t)((dwParam1 >> 8) & 0xFF);
+        int32_t data2 = (int32_t)((dwParam1 >> 16) & 0xFF);
 
-        // Post the event to LabVIEW
-        PostLVUserEvent(*g_userEventRef, &midiData);
+        // Check if the status is in the filter array
+        if (std::find(statusFilterArray.begin(), statusFilterArray.end(), static_cast<uint8_t>(status)) != statusFilterArray.end()) {
+            // Status is in the array, send the event to LabVIEW
+            MidiData midiData;
+            midiData.Status = status;
+            midiData.data1 = data1;
+            midiData.data2 = data2;
+
+            // Post the event to LabVIEW
+            PostLVUserEvent(*g_userEventRef, &midiData);
+        }
+        // Else: Discard the message
     }
+}
+
+// DLL export function to set the Status filter array
+extern "C" __declspec(dllexport) void LVSetStatusFilterArray(uint8_t* filterArray, int32_t arraySize)
+{
+    // Clear the current filter and set the new one
+    statusFilterArray.clear();
+    statusFilterArray.insert(statusFilterArray.end(), filterArray, filterArray + arraySize);
 }
 
 // DLL export functions
